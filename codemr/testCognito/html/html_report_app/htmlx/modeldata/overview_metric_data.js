e6dc49f69462866c14540d910840d78e6c5714f1
peloo <pellandamatteo@gmail.com>
2019-05-09 10:09:21 +0200

    Pulizia Repository App

--- codemr/testCognito/html/html_report_app/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"testCognito working set: {app}", "Number of Highly Problematic Classes":0, "Number of Entities":9, "Number of Problematic Classes":0, "Class Lines of Code":307, "Number of External Packages":5, "Number of Packages":1, "Number of External Entities":17}
--- codemr/testCognito/html/main_report/htmlx/modeldata/packagedependency.js
+++
@@ -1,5 +0,0 @@
-var matrix = [[0]];
-var packages = [{
-"name": " com.amazonaws.amplify.generated.graphql", "color": " #3182bd"
-}
-];
