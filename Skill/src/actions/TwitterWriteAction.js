b2cf89dc22325f6477515ebebe7a014ee34c771f
AlessandroPegoraro <spampego@gmail.com>
2019-05-14 12:35:03 +0200

    aggiunta conferma twitter

--- Skill/src/actions/TwitterWriteAction.js
+++ Skill/src/actions/TwitterWriteAction.js
@@ -1,11 +1,5 @@
 const { Action } = require('./Action.js');
 const Twitter = require('twitter');
-const twitter = new Twitter({
-  consumer_key: 'sFOOM7Ln3yEF3pzwibMv16OKs',
-  consumer_secret: '6SLqOZxNV22gDOmPSSJKQSeWWHfGVKwuk2aTf78HO0qfipwaof',
-  access_token_key: '1110508480543248384-kkSx42K1rEjMeV4NA6YFVFKvvZGHXm',
-  access_token_secret: 'Ch95zS5FfL6wiuaFOYl25Z83KcDjIm0VsXq9QpUmBQ4HE'
-});
 
 var exports = module.exports = {};
 
@@ -33,21 +27,33 @@ class TwitterWriteAction extends Action {
         });
         console.log("un sacco");*/
         
-        if(this.params.length<2) {
-            console.log("Twitter params <2");
-            check.output = "Per pubblicare un tweet, dimmi <emphasis level=\"moderate\">invia</emphasis> seguito dal corpo del messaggio";
+        if(this.params.length==1) {
+            check.output = "Dimmi che tweet vuoi pubblicare?";
             check.noInput= false;
-        } else {
-            console.log("Twitter ha params input");
-            check.output = 'Tweet inviato con successo';
-            let body = this.params[1];
-            await twitter.post('statuses/update', {status: body}).then(data => {
-                console.log(data);
-            }, err => {
-                console.log(err);
-            });
-        }
-        
+        } else if(this.params.length==2){
+        	check.output = "confermi "+this.params[1]+" ?";
+        	check.noInput= false;
+        }else if(this.params.length==2){
+        	if(this.params[2]!="no"){
+        		const twitter = new Twitter({
+          		  consumer_key: 'sFOOM7Ln3yEF3pzwibMv16OKs',
+          		  consumer_secret: '6SLqOZxNV22gDOmPSSJKQSeWWHfGVKwuk2aTf78HO0qfipwaof',
+          		  access_token_key: '1110508480543248384-kkSx42K1rEjMeV4NA6YFVFKvvZGHXm',
+          		  access_token_secret: 'Ch95zS5FfL6wiuaFOYl25Z83KcDjIm0VsXq9QpUmBQ4HE'
+          		});
+        		check.output = 'Tweet inviato con successo';
+        		let body = this.params[1];
+        		await twitter.post('statuses/update', {status: body}).then(data => {
+        			console.log(data);
+        		}, err => {
+        			console.log(err);
+        		});
+        	}else{
+        		this.params.length =  1;
+        		check.output = "Dimmi che tweet vuoi pubblicare?";
+        		check.noInput= false;
+        	}        	
+        }        
         return check;
     }
 }
