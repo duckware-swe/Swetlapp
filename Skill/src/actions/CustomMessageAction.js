3f20c9ee7f4564a0f5d2b4637e71eb35f0479e3a
AlessandroPegoraro <spampego@gmail.com>
2019-05-14 16:58:19 +0200

    fix finale

--- Skill/src/actions/CustomMessageAction.js
+++ Skill/src/actions/CustomMessageAction.js
@@ -11,7 +11,7 @@ class CustomMessageAction extends Action {
     async run() {
     	let check = {
         		output: '',
-        		noInput: 'DEFAULT'
+        		slotReq: 'DEFAULT'
         };
         this.params.forEach(param => {
             //console.log("Parametro: " + param);
