686b1084e815e136341b1afe6fca006537d55252
AlessandroPegoraro <spampego@gmail.com>
2019-05-14 18:29:03 +0200

    fix

--- Skill/src/utils/PhraseGenerator.js
+++ Skill/src/utils/PhraseGenerator.js
@@ -8,79 +8,60 @@ var exports = module.exports = {};
  */
 //TODO: serve il name nelle Actions???
 function phraseGenerator(name, param) {
+	let phrase = [];
     switch (name) {
     	case "no_auth":
-    		const phrase = [
-    			"Devi autenticarti con il tuo account <lang xml:lang=\"en-US\">SwetlApp</lang> per usare questa skill. Ti ho inviato le istruzioni nella tua <lang xml:lang=\"en-US\">App Alexa</lang>.",
-    			"Mi dispiace ma per usare <lang xml:lang=\"en-US\">SwetlApp</lang> devi prima autenticarti. Se non sai come eseguire l\'autenticazione segui le istruzioni nella tua <lang xml:lang=\"en-US\">App Alexa</lang>."
-    		];
-    		return phrase[Math.floor(Math.random() * phrase.length)];
+    		phrase.push("Devi autenticarti con il tuo account <lang xml:lang=\"en-US\">SwetlApp</lang> per usare questa skill. Ti ho inviato le istruzioni nella tua <lang xml:lang=\"en-US\">App Alexa</lang>.");
+    		phrase.push("Mi dispiace ma per usare <lang xml:lang=\"en-US\">SwetlApp</lang> devi prima autenticarti. Se non sai come eseguire l\'autenticazione segui le istruzioni nella tua <lang xml:lang=\"en-US\">App Alexa</lang>.");
     	case "start" 
-    		const phrase = [
-    	        "Ciao "+param+", benvenuto in <lang xml:lang=\"en-US\">SwetlApp</lang>, come posso aiutarti?",
-    	        "Benvenuto in <lang xml:lang=\"en-US\">SwetlApp</lang>",
-    	        "Ciao "+param+" sei in <lang xml:lang=\"en-US\">SwetlApp</lang>, sono qui per aiutarti",
-    	        "Benvenuto "+param+", sei in <lang xml:lang=\"en-US\">SwetlApp</lang>, come posso aiutarti?",
-    	        "Ciao "+param" sei in <lang xml:lang=\"en-US\">SwetlApp</lang>, prova a far partire un <lang xml:lang=\"en-US\">workflow</lang>?"
-    	    ],
-    		return phrase[Math.floor(Math.random() * phrase.length)];
+    		phrase.push("Ciao "+param+", benvenuto in <lang xml:lang=\"en-US\">SwetlApp</lang>, come posso aiutarti?");
+    		phrase.push("Benvenuto in <lang xml:lang=\"en-US\">SwetlApp</lang>");
+    		phrase.push("Ciao "+param+" sei in <lang xml:lang=\"en-US\">SwetlApp</lang>, sono qui per aiutarti");
+    		phrase.push("Benvenuto "+param+", sei in <lang xml:lang=\"en-US\">SwetlApp</lang>, come posso aiutarti?");
+    		phrase.push("Ciao "+param" sei in <lang xml:lang=\"en-US\">SwetlApp</lang>, prova a far partire un <lang xml:lang=\"en-US\">workflow</lang>?");
     	case "start_WF" 
-			const phrase = [
-		        "Va bene, eseguo "+param+" ",
-		        "Eseguo "+param+" ",
-		        "Comincio "+param+" ",
-		        "Per cominciare ",
-		        "Inizio "+param+" ",
-		        "Comincio il <lang xml:lang=\"en-US\">workflow</lang> "+param+" "
-		    ],
-			return phrase[Math.floor(Math.random() * phrase.length)];
+    		phrase.push("Va bene, eseguo "+param+" ");
+    		phrase.push("Eseguo "+param+" ");
+    		phrase.push("Comincio "+param+" ");
+    		phrase.push("Per cominciare ");
+    		phrase.push("Inizio "+param+" ");
+    		phrase.push("Comincio il <lang xml:lang=\"en-US\">workflow</lang> "+param+" ");
         case "read_feed":
-        	const phrase = [
-        		"Queste sono le notizie da "+ param + ": ",
-        		"Ultime notizie di "+param+": ",
-        		"Le ultime notizie di "+param+ " sono: ",
-        		"Ecco le notizie da "+param+": "
-        	]
-            return phrase[Math.floor(Math.random() * phrase.length)];
+        	phrase.push("Queste sono le notizie da "+ param + ": ");
+        	phrase.push("Ultime notizie di "+param+": ");
+        	phrase.push("Le ultime notizie di "+param+ " sono: ");
+        	phrase.push("Ecco le notizie da "+param+": ");
         case "read_tweet":
-        	const phrase = [
-        		"Questi sono gli ultimi <lang xml:lang=\"en-US\">Tweet</lang> da "+ param + ": ",
-        		"Ultimi <lang xml:lang=\"en-US\">Tweet</lang> di "+param+": ",
-        		"Gli ultimi <lang xml:lang=\"en-US\">Tweet</lang> di "+param+ " sono: ",
-        		"Ecco i <lang xml:lang=\"en-US\">Tweet</lang> pubblicati da "+param+": "
-        	]
-            return phrase[Math.floor(Math.random() * phrase.length)];
+        	phrase.push("Questi sono gli ultimi <lang xml:lang=\"en-US\">Tweet</lang> da "+ param + ": ");
+        	phrase.push("Ultimi <lang xml:lang=\"en-US\">Tweet</lang> di "+param+": ");
+        	phrase.push("Gli ultimi <lang xml:lang=\"en-US\">Tweet</lang> di "+param+ " sono: ");
+        	phrase.push("Ecco i <lang xml:lang=\"en-US\">Tweet</lang> pubblicati da "+param+": ");
         case "write_tweet":
-        	const phrase = [
-        		"Dimmi che <lang xml:lang=\"en-US\">Tweet</lang> vuoi pubblicare?",
-        		"Che cosa vuoi pubblicare?",
-        		"Dimmi che cosa vuoi pubblicare?",
-        		"Che <lang xml:lang=\"en-US\">Tweet</lang> vuoi inviare?",
-        		"Dimmi il testo del <lang xml:lang=\"en-US\">Tweet</lang> che vuoi inviare?"
-        	]
-            return phrase[Math.floor(Math.random() * phrase.length)];
+        	phrase.push("Dimmi che <lang xml:lang=\"en-US\">Tweet</lang> vuoi pubblicare?");
+        	phrase.push("Che cosa vuoi pubblicare?");
+        	phrase.push("Dimmi che cosa vuoi pubblicare?");
+        	phrase.push("Che <lang xml:lang=\"en-US\">Tweet</lang> vuoi inviare?");
+        	phrase.push("Dimmi il testo del <lang xml:lang=\"en-US\">Tweet</lang> che vuoi inviare?");
         case "confirm_tweet":
-        	const phrase = [
-        		param+", è questo quello che vuoi pubblicare?",
-        		param+", è questo quello che vuoi inviare?",
-        		param+", confermi l\'invio?",
-        		"Mi confermi di aver detto: "+param+"?",
-        		"Hai detto: "+param+", giusto?",
-        		"Confermi: "+param+"?",
-        		"Vuoi inviare: "+param+"?"
-        	]
-            return phrase[Math.floor(Math.random() * phrase.length)];
+        	phrase.push(param+", è questo quello che vuoi pubblicare?");
+        	phrase.push(param+", è questo quello che vuoi inviare?");
+        	phrase.push(param+", confermi l\'invio?");
+        	phrase.push("Mi confermi di aver detto: "+param+"?");
+        	phrase.push("Hai detto: "+param+", giusto?");
+        	phrase.push("Confermi: "+param+"?");
+        	phrase.push("Vuoi inviare: "+param+"?");
         case "tv_schedule":
-            return new TVScheduleAction(name, params);
+        	phrase.push(" ");
         case "weather":
-        	return new WeatherAction(name, params);
+        	phrase.push(" ");
         case "add_trello_cards":
-            return new AddCardTrelloAction(name,params);
+        	phrase.push(" ");
         case "read_trello_cards":
-            return new GetCardsFromBoardTrelloAction(name,params);
+        	phrase.push(" ");
         default :
             throw "Unknown action";
     }
+    return phrase[Math.floor(Math.random() * phrase.length)];
 }
 
 exports.actionFactory = actionFactory;
