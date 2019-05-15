4a530e1cb64dd0a636f7490f27a625fe3584c492
AlessandroPegoraro <spampego@gmail.com>
2019-05-15 21:58:58 +0200

    aggiornato twitter write

--- Skill/src/actions/TwitterWriteAction.js
+++ Skill/src/actions/TwitterWriteAction.js
@@ -20,33 +20,33 @@ class TwitterWriteAction extends Action {
         		slotReq: 'DEFAULT'
         };
         
-        if(this.params.length==1) {
+        if(this.params.length==2) {
             check.output = phraseGenerator("write_tweet");
             check.slotReq= 'tweetBody';
-        } else if(this.params.length==2){
-        	check.output = phraseGenerator("confirm_tweet",this.params[1]);
+        } else if(this.params.length==3){
+        	check.output = phraseGenerator("confirm_tweet",this.params[2]);
         	check.slotReq= 'confirmitionSlot';
-        }else if(this.params.length==3){
-        	if(this.params[2]!="si" && this.params[2]!="no"){
-        		check.output = phraseGenerator("confirm_tweet",this.params[1]);
-        		this.params.length =  2;
+        }else if(this.params.length==4){
+        	if(this.params[3]!="si" && this.params[3]!="no"){
+        		check.output = phraseGenerator("confirm_tweet",this.params[2]);
+        		this.params.length =  3;
             	check.slotReq= 'confirmitionSlot';
-        	}else if(this.params[2]=="si"){
+        	}else if(this.params[3]=="si"){
         		const twitter = new Twitter({
-          		  consumer_key: 'sFOOM7Ln3yEF3pzwibMv16OKs',
-          		  consumer_secret: '6SLqOZxNV22gDOmPSSJKQSeWWHfGVKwuk2aTf78HO0qfipwaof',
-          		  access_token_key: '1110508480543248384-kkSx42K1rEjMeV4NA6YFVFKvvZGHXm',
-          		  access_token_secret: 'Ch95zS5FfL6wiuaFOYl25Z83KcDjIm0VsXq9QpUmBQ4HE'
-          		});
+        			  consumer_key: 'sFOOM7Ln3yEF3pzwibMv16OKs',
+        			  consumer_secret: '6SLqOZxNV22gDOmPSSJKQSeWWHfGVKwuk2aTf78HO0qfipwaof',
+              		  access_token_key: this.params[0],
+              		  access_token_secret: this.params[1]
+        			});        		
         		check.output = phraseGenerator("tweet_success");
-        		let body = this.params[1];
+        		let body = this.params[2];
         		await twitter.post('statuses/update', {status: body}).then(data => {
         			console.log(data);
         		}, err => {
         			console.log(err);
         		});
         	}else{
-        		this.params.length =  1;
+        		this.params.length =  2;
         		check.output = phraseGenerator("write_tweet");
         		check.slotReq = 'tweetBody';
         	}        	
